e6a04fad4f4b9026cf88634dd4f0e07271dfaba2
Collins Rotich <14885955+collinskrotich@users.noreply.github.com>
2023-08-25 09:15:05 +0300

    Sightings Dao

---
+++ src/main/java/org/crotich/dao/SightingsDao.java
@@ -0,0 +1,29 @@
+package org.crotich.dao;
+
+import org.crotich.config.DatabaseConfig;
+import org.crotich.models.Sightings;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
+
+public class SightingsDao {
+
+    private static final Sql2o sql2o = DatabaseConfig.getDatabaseProduction();
+    private static Connection connection = sql2o.open();
+
+    public static boolean  createSighting(Sightings sightings) {
+        try {
+            String query = "INSERT INTO sightings ( animal_id ,location, ranger_name) VALUES (:animalId,:location, :rangerName);";
+            connection.createQuery(query)
+                    .addParameter("animalId", sightings.getAnimal_id())
+                    .addParameter("location", sightings.getLocation())
+                    .addParameter("rangerName",sightings.getRangerName())
+                    .executeUpdate();
+            return true;
+        } catch (Exception exception) {
+            System.out.println(exception.getMessage());
+            return false;
+        }
+    }
+
+
+}
